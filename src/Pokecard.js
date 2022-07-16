fe8bce6f07dc80f718871bb0fc5671a6a1f33693
Glory Omosomwan <gloryomosomwan@gmail.com>
2022-07-16 16:42:23 -0600

    Added some more styling

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -9,7 +9,9 @@ class Pokecard extends Component {
         return (
             <div className='Pokecard'>
                 <h1>{this.props.name}</h1>
-                <img src={imgSrc} />
+                <div className='Pokecard-image'>
+                    <img src={imgSrc} />
+                </div>
                 <div className='Pokecard-data'>EXP: {this.props.exp}</div>
                 <div className='Pokecard-data'>Type: {this.props.type}</div>
             </div>
