501dfb4ad8823cc45609a7052841a354458560fd
Glory Omosomwan <gloryomosomwan@gmail.com>
2022-07-16 16:21:52 -0600

    Pokegame logic implemented

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import Pokedex from './Pokedex';
 import React, { Component } from 'react';
+import Pokegame from './Pokegame';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Pokedex />
+        <Pokegame />
       </div>
     );
   }
